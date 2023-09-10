4d36c2168ad7ae20d198714f9df9bddd73026769
vision-from-touch <susan-zjc@sjtu.edu.cn>
2023-09-09 21:32:35 -0400

    change teaser and title

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeImage() {
+    document.getElementById('out').src = './static/images/o1.png';
+}
+
+function revertImage() {
+    document.getElementById('out').src = './static/images/r1.png';
+}
